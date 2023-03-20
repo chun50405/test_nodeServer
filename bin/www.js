78c054f03ede9a7fa05ccafc7b7e88071b07eedb
chun50405 <chun50405@gmail.com>
2023-03-20 10:32:22 +0800

    使用cluster模組 來使用多核心 增加效能

--- bin/www.js
+++ bin/www.js
@@ -9,6 +9,8 @@ const startConfig = require('../config/startConfig')[env];
 const models = require('../models');
 const sequelize = models.sequelize;
 const fs = require('fs')
+const cluster = require('cluster');
+const os = require('os');
 
 /**
  * Get port from environment and store in Express.
@@ -21,55 +23,82 @@ app.set('port', port);
  * Create HTTP server.
  */
 
-if(startConfig.useHttps){
-  let HTTPS_CONFIG = startConfig.httpsConfig;
-  let path = require('path');
-  let certDir = HTTPS_CONFIG.certDir;
-  let ca = [];
-  let server_key = path.join(__dirname, certDir, HTTPS_CONFIG.key)
-  let server_cert = path.join(__dirname, certDir,HTTPS_CONFIG.cert)
-  if(fs.existsSync(server_key) && fs.existsSync(server_cert)) {
-    for(let idx in HTTPS_CONFIG.ca){
-      let caFile = HTTPS_CONFIG.ca[idx];
-      let server_ca = path.join(__dirname, certDir, caFile)
-      if(fs.existsSync(server_ca)) {
-        ca.push(fs.readFileSync(server_ca))
-      }
-    }
-    server = require('https').createServer({
-      key: fs.readFileSync(server_key),
-      cert: fs.readFileSync(server_cert),
-      // passphrase: HTTPS_CONFIG.passphrase,
-      // ca: ca,
-      // secureOptions: crypto.constants.SSL_OP_NO_TLSv1 | crypto.constants.SSL_OP_NO_TLSv1_1
-    }, app);
-    console.log('USE HTTPS');
-  } else {
-    console.log('no key & cert File')
-    server = require('http').createServer(app);
-  }
 
-}else{
-  server = require('http').createServer(app);
+ if(startConfig.useHttps){
+   let HTTPS_CONFIG = startConfig.httpsConfig;
+   let path = require('path');
+   let certDir = HTTPS_CONFIG.certDir;
+   let ca = [];
+   let server_key = path.join(__dirname, certDir, HTTPS_CONFIG.key)
+   let server_cert = path.join(__dirname, certDir,HTTPS_CONFIG.cert)
+   if(fs.existsSync(server_key) && fs.existsSync(server_cert)) {
+     for(let idx in HTTPS_CONFIG.ca){
+       let caFile = HTTPS_CONFIG.ca[idx];
+       let server_ca = path.join(__dirname, certDir, caFile)
+       if(fs.existsSync(server_ca)) {
+         ca.push(fs.readFileSync(server_ca))
+       }
+     }
+     server = require('https').createServer({
+       key: fs.readFileSync(server_key),
+       cert: fs.readFileSync(server_cert),
+       // passphrase: HTTPS_CONFIG.passphrase,
+       // ca: ca,
+       // secureOptions: crypto.constants.SSL_OP_NO_TLSv1 | crypto.constants.SSL_OP_NO_TLSv1_1
+     }, app);
+     console.log('USE HTTPS');
+   } else {
+     console.log('no key & cert File')
+     server = require('http').createServer(app);
+   }
+
+ }else{
+   server = require('http').createServer(app);
+ }
+//
+if(cluster.isMaster) {
+  // Fork workers.
+  for (let i = 0; i < os.cpus().length; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+
+  sequelize.sync({force: false })
+  .then(function(){
+   console.log('testServer port =', port)
+   console.log('Sequelize table create ok!')
+  })
+  .catch((error) => {
+    console.log('error=', error)
+  })
+} else {
+  server.listen(port, function() {
+   debug('Express server listening on port ' + server.address().port);
+  });
 }
+
+
 
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 // ******************************** 手動新增 S ************************************
-sequelize.sync({force: false })
-.then(function(){
- console.log('testServer port =', port)
- console.log('Sequelize table create ok!')
-
- server.listen(port, function() {
-  debug('Express server listening on port ' + server.address().port);
- });
-})
-.catch((error) => {
-  console.log('error=', error)
-})
+// sequelize.sync({force: false })
+// .then(function(){
+//  console.log('testServer port =', port)
+//  console.log('Sequelize table create ok!')
+//
+//  server.listen(port, function() {
+//   debug('Express server listening on port ' + server.address().port);
+//  });
+// })
+// .catch((error) => {
+//   console.log('error=', error)
+// })
 // ******************************** 手動新增 E ************************************
 
 //server.listen(port);
--- routes/indexController.js
+++ routes/indexController.js
@@ -4,8 +4,12 @@ const models = require('../models');
 const sequelize = models.sequelize;
 const Sequelize = models.Sequelize;
 const Op = sequelize.Op;
+const cluster = require('cluster');
 router.get('/', (req, res, next) => {
-	res.json('Welcome!!!')
+	// const workerId = cluster.worker.id;
+	console.log(Math.random());
+  // res.json({ workerId: workerId, message: 'Hello from worker ' + workerId , num: Math.random()});
+	res.json({num: Math.random()})
 })
 
 
