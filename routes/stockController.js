6942c636dda2a0886db6b773e411a6a53c633fad
chun50405 <chun50405@gmail.com>
2023-04-26 17:04:28 +0800

    增加get/put groupStock.json的方法

--- routes/stockController.js
+++ routes/stockController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
+const fs = require('fs/promises');
 const superagent = require('superagent');
 const cheerio = require('cheerio');
 const twseUrl = "https://mis.twse.com.tw/stock/api/getStockInfo.jsp";
@@ -32,6 +32,34 @@ router.get('/news', async (req, res, next) => {
 	}
 })
 
+router.get('/getStockGroup', async (req, res, next) => {
+	try {
+		let result = await fs.readFile('./json/groupStock.json');
 
+		res.send(JSON.parse(result));
+	} catch (e) {
+		console.log('e=', e)
+		res.status(500).send(e)
+	}
+
+})
+
+router.put('/editStockGroup', async (req, res, next) => {
+	try {
+		let body = req.body;
+		let jsonData = body.jsonData;
+		let groupName = body.groupName;
+		let theStockGroup = await fs.readFile('./json/groupStock.json');
+
+		let data = JSON.parse(theStockGroup)
+		data[groupName] = jsonData
+
+		await fs.writeFile('./json/groupStock.json', JSON.stringify(data));
+		res.send({message: 'writeFile Success'});
+	} catch (e) {
+		console.log('e=', e)
+		res.status(500).send(e);
+	}
+})
 
 module.exports = router;
